caeb7ec7db10afeb13af701850c29d22c1480f8a
kurtiz384 <kurtiz@seznam.cz>
2026-01-13 18:37:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -437,7 +437,7 @@ class TextToSpeechApp {
         const ssml = `
 <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='${locale}'>
     <voice name='${voiceId}'>
-        <break time='100ms'/>
+        <break time='300ms'/>
         <prosody rate='${rate}'>${safeText}</prosody>
     </voice>
 </speak>
